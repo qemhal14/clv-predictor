186969df30d59d76788910349c5494390bfe785e
qemhal14 <124538307+qemhal14@users.noreply.github.com>
2024-08-25 17:04:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.info("This is a machine learning app to predict customer lifetime value.")
 # Manual input for single prediction
 with st.sidebar:
   st.header("Input Features"):
-  State = st.selectbox("State",("Arizona", "California", 'Nevada", "Oregon", "Washington"))
+  State = st.selectbox("State",("Arizona", "California", "Nevada", "Oregon", "Washington"))
   Response = st.selectbox("Response",("Yes", "No"))
   Coverage = st.selectbox("Coverage",("Basic", "Premium", "Extended"))
   Education = st.selectbox("Education",("High School or Below", "College", "Bachelor", "Master", "Doctor"))
