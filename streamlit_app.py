684aa8249fa94b4eb5eb9cc218cd26f3c2a1da04
qemhal14 <124538307+qemhal14@users.noreply.github.com>
2024-08-25 16:39:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("🚗 Auto Insurance Company")
-st.info("This is a machine learning app to predict customer lifetime value)
+st.info("This is a machine learning app to predict customer lifetime value.")
