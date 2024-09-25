5e1e0e7fbab5a7742db6b185254b83a76a5199ac
qemhal14 <124538307+qemhal14@users.noreply.github.com>
2024-09-25 17:30:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,6 +84,6 @@ remaining_df = input_df[['Income', 'Monthly Premium Auto', 'Months Since Last Cl
 final_df = pd.concat([encoded_ordinal_df, encoded_onehot_df, remaining_df], axis=1)
 
 st.write("Encoded Data for Prediction:")
-st.write(final_df)
+final_df
 
   
