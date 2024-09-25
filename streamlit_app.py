0c3c25e3bbd54b2c24bab600ed5518f3ba46cb35
qemhal14 <124538307+qemhal14@users.noreply.github.com>
2024-09-25 18:46:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,57 +7,57 @@ st.info("This is a machine learning app to predict customer lifetime value.")
 
 # Manual input for single prediction
 with st.sidebar:
-  st.header("Input Data")
-  State = st.selectbox("State",("Arizona", "California", "Nevada", "Oregon", "Washington"))
-  Response = st.selectbox("Response",("Yes", "No"))
-  Coverage = st.selectbox("Coverage",("Basic", "Premium", "Extended"))
-  Education = st.selectbox("Education",("High School or Below", "College", "Bachelor", "Master", "Doctor"))
-  EmploymentStatus = st.selectbox("Employment",("Employed", "Unemployed", "Medical Leave", "Disabled", "Retired"))
-  Gender = st.selectbox("Gender",("Male", "Female"))
-  Income = st.slider("Income", 10037.00, 99981.00, 34080.00)
-  LocationCode = st.selectbox("Location",("Suburban", "Rural", "Urban"))
-  MaritalStatus = st.selectbox("Matiral Status",("Single", "Married", "Divorced"))
-  MonthlyPremiumAuto = st.slider("Monthly Premium", 61.0, 298.0, 83.0)
-  MonthsSinceLastClaim = st.slider("Months Since Last Claim", 0, 35, 14)
-  MonthsSincePolicyInception = st.slider("Months Since Policy Inception", 0, 99, 48)
-  NumberofOpenComplaints = st.slider("Number of Open Complaints", 0, 5, 1)
-  NumberofPolicies = st.slider("Number of Policies", 1, 9, 2)
-  PolicyType = st.selectbox("Policy Type",("Personal Auto", "Special Auto", "Corporate Auto"))
-  Policy = st.selectbox("Policy",("Personal L1", "Personal L2", "Personal L3", "Special L1", "Special L2", "Special L3", "Corporate L1", "Corporate L2", "Corporate L3"))
-  RenewOfferType = st.selectbox("Renew Offer",("Offer1", "Offer2", "Offer3", "Offer4"))
-  SalesChannel = st.selectbox("Sales Channel",("Agent", "Branch", "Web", "Call Center"))
-  TotalClaimAmount = st.slider("Total Claim Amount", 0.00, 2893.23, 384.00)
-  VehicleClass = st.selectbox("Vehicle Class",("Four-Door Car", "Two-Door Car", "SUV", "Sports Car", "Luxury SUV", "Luxury Car"))
-  VehicleSize = st.selectbox("Vehicle Size",("Small", "Medsize", "Large"))
+    st.header("Input Data")
+    State = st.selectbox("State", ("Arizona", "California", "Nevada", "Oregon", "Washington"))
+    Response = st.selectbox("Response", ("Yes", "No"))
+    Coverage = st.selectbox("Coverage", ("Basic", "Premium", "Extended"))
+    Education = st.selectbox("Education", ("High School or Below", "College", "Bachelor", "Master", "Doctor"))
+    EmploymentStatus = st.selectbox("Employment", ("Employed", "Unemployed", "Medical Leave", "Disabled", "Retired"))
+    Gender = st.selectbox("Gender", ("Male", "Female"))
+    Income = st.slider("Income", 10037.00, 99981.00, 34080.00)
+    LocationCode = st.selectbox("Location", ("Suburban", "Rural", "Urban"))
+    MaritalStatus = st.selectbox("Marital Status", ("Single", "Married", "Divorced"))
+    MonthlyPremiumAuto = st.slider("Monthly Premium", 61.0, 298.0, 83.0)
+    MonthsSinceLastClaim = st.slider("Months Since Last Claim", 0, 35, 14)
+    MonthsSincePolicyInception = st.slider("Months Since Policy Inception", 0, 99, 48)
+    NumberofOpenComplaints = st.slider("Number of Open Complaints", 0, 5, 1)
+    NumberofPolicies = st.slider("Number of Policies", 1, 9, 2)
+    PolicyType = st.selectbox("Policy Type", ("Personal Auto", "Special Auto", "Corporate Auto"))
+    Policy = st.selectbox("Policy", ("Personal L1", "Personal L2", "Personal L3", "Special L1", "Special L2", "Special L3", "Corporate L1", "Corporate L2", "Corporate L3"))
+    RenewOfferType = st.selectbox("Renew Offer", ("Offer1", "Offer2", "Offer3", "Offer4"))
+    SalesChannel = st.selectbox("Sales Channel", ("Agent", "Branch", "Web", "Call Center"))
+    TotalClaimAmount = st.slider("Total Claim Amount", 0.00, 2893.23, 384.00)
+    VehicleClass = st.selectbox("Vehicle Class", ("Four-Door Car", "Two-Door Car", "SUV", "Sports Car", "Luxury SUV", "Luxury Car"))
+    VehicleSize = st.selectbox("Vehicle Size", ("Small", "Medsize", "Large"))
 
-# Create Dataframe for Input Features
+# Create DataFrame for Input Features
 data = {
-  "State":State,
-  "Response":Response,
-  "Coverage":Coverage,
-  "Education":Education,
-  "EmploymentStatus":EmploymentStatus,
-  "Gender":Gender,
-  "Income":Income,
-  "Location Code":LocationCode,
-  "Marital Status":MaritalStatus,
-  "Monthly Premium Auto":MonthlyPremiumAuto,
-  "Months Since Last Claim":MonthsSinceLastClaim,
-  "Months Since Policy Inception":MonthsSincePolicyInception,
-  "Number of Open Complaints":NumberofOpenComplaints,
-  "Number of Policies":NumberofPolicies,
-  "Policy Type":PolicyType,
-  "Policy":Policy,
-  "Renew Offer Type":RenewOfferType,
-  "Sales Channel":SalesChannel,
-  "Total Claim Amount":TotalClaimAmount,
-  "Vehicle Class":VehicleClass,
-  "Vehicle Size":VehicleSize
+    "State": State,
+    "Response": Response,
+    "Coverage": Coverage,
+    "Education": Education,
+    "EmploymentStatus": EmploymentStatus,
+    "Gender": Gender,
+    "Income": Income,
+    "Location Code": LocationCode,
+    "Marital Status": MaritalStatus,
+    "Monthly Premium Auto": MonthlyPremiumAuto,
+    "Months Since Last Claim": MonthsSinceLastClaim,
+    "Months Since Policy Inception": MonthsSincePolicyInception,
+    "Number of Open Complaints": NumberofOpenComplaints,
+    "Number of Policies": NumberofPolicies,
+    "Policy Type": PolicyType,
+    "Policy": Policy,
+    "Renew Offer Type": RenewOfferType,
+    "Sales Channel": SalesChannel,
+    "Total Claim Amount": TotalClaimAmount,
+    "Vehicle Class": VehicleClass,
+    "Vehicle Size": VehicleSize
 }
 
-st.write("Inputed Single Data for Prediction.")
+st.write("Inputted Single Data for Prediction.")
 input_df = pd.DataFrame(data, index=[0])
-input_df
+st.write(input_df)
 
 # Encoding
 onehot_columns = ['State', 'Response', 'Coverage', 'EmploymentStatus', 'Gender', 'Location Code', 'Marital Status', 'Policy Type', 'Policy', 'Renew Offer Type', 'Sales Channel', 'Vehicle Size']
@@ -74,8 +74,10 @@ encoded_ordinal_df = pd.DataFrame(encoded_ordinal, columns=ordinal_columns)
 
 # One-hot encoding
 onehot_encoder = OneHotEncoder(drop='first', sparse_output=False)
-encoded_onehot = onehot_encoder.fit_transform(input_df[onehot_columns])
-encoded_onehot_df = pd.DataFrame(encoded_onehot)
+encoded_onehot = onehot_encoder.fit_transform(input_df[onehot_columns])  # Convert to dense array
+
+# Create a DataFrame from the one-hot encoded data
+encoded_onehot_df = pd.DataFrame(encoded_onehot, columns=onehot_encoder.get_feature_names_out(onehot_columns))
 
 # Resetting index to avoid issues in concatenation
 encoded_ordinal_df.reset_index(drop=True, inplace=True)
@@ -83,10 +85,8 @@ encoded_onehot_df.reset_index(drop=True, inplace=True)
 remaining_df = input_df[['Income', 'Monthly Premium Auto', 'Months Since Last Claim', 'Months Since Policy Inception', 'Number of Open Complaints', 'Number of Policies', 'Total Claim Amount']]
 remaining_df.reset_index(drop=True, inplace=True)
 
-# Combine original non-encoded columns with the encoded one-hot columns
-final_df = pd.concat([encoded_onehot_df, encoded_ordinal_df, remaining_df], axis=1)
+# Combine all encoded columns and the remaining numeric columns
+final_df = pd.concat([encoded_ordinal_df, encoded_onehot_df, remaining_df], axis=1)
 
 st.write("Encoded Data for Prediction:")
-st.write(encoded_onehot)
 st.write(final_df)
-  
